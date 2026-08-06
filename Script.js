2f3424812ccb03940121816199296b0cf9dba966
gabriellatronquin-source <gabriellatronquin@gmail.com>
2026-08-06 18:20:26 -0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,14 @@
+
+
+function mostrarMenu(){
+
+    let menu = document.getElementById("menuPesquisa");
+
+    if(menu.style.display=="block"){
+        menu.style.display="none";
+    }else{
+        menu.style.display="block";
+    }
+
+}
+
